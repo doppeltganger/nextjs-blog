5dbac6ecd6e98154e3a567f29ae66a4bc3c9a6f3
doppeltganger <stanislav.danylov@gmail.com>
2022-12-21 18:28:23 +0200

    add updated app

---
+++ components/TitleBlock.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const TitleBlock = () => {
+  return (
+  <>
+    <h1>First Post</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti in alias, error consequatur aperiam iusto ab provident quidem nisi rem voluptatibus cum saepe qui mollitia et! Eaque aspernatur tempora fugiat itaque quia nostrum ab quo voluptate non autem, blanditiis maxime eveniet esse corporis eum omnis nihil quod dolorem, mollitia placeat!</p>
+  </>
+  );
+};
